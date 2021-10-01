e7a65c8a7163b1f80f0e0293cef8339b783744b1
Largemain, Stephane <stephane.largemain@clarivate.com>
2021-10-01 09:44:57 +0200

    Sort json in alphabetical order (high priority) and case sensitive order (lower priority)

--- src/post-processors/sort-by-key.post-processor.ts
+++ src/post-processors/sort-by-key.post-processor.ts
@@ -5,6 +5,25 @@ export class SortByKeyPostProcessor implements PostProcessorInterface {
 	public name: string = 'SortByKey';
 
 	public process(draft: TranslationCollection, extracted: TranslationCollection, existing: TranslationCollection): TranslationCollection {
-		return draft.sort();
+		return draft.sort((obj1, obj2) => {
+			for (let i = 0; i < obj1.length; i++) {
+				const char1 = obj1[i];
+				const char2 = obj2[i];
+
+				if (!char2) {
+					return 1;
+				}
+
+				if (char1 !== char2) {
+					if (char1.toLowerCase() === char2.toLowerCase()) {
+						return char1 < char2 ? -1 : 1;
+					} else {
+						return char1.toLowerCase() < char2.toLowerCase() ? -1 : 1;
+					}
+				}
+			}
+
+			return obj1.length < obj2.length ? -1 : 1;
+		});
 	}
 }
